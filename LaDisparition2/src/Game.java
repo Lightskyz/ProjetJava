cda2ec8a7205b7d433b94ea1f12a0175240c0f12
Antoine <antoine.rakotozafy@isep.fr>
2015-01-15 21:46:06 +0100

    Clean main -- remove player, replaceby gardien, infiltrant

--- LaDisparition2/src/Game.java
+++ LaDisparition2/src/Game.java
@@ -1,7 +1,5 @@
 import java.awt.event.KeyEvent;
 
-
-
 public class Game {
 	
 	public static final int scale = 2; //echelle
@@ -10,99 +8,19 @@ public class Game {
 	public static final float WIDTH = 28f; //taille d'un carré de ma map
 	
 	private static Map map; //j'appelle la map dans Game
-	private static Player player1;//j'appelle le joueur1 dans Game
-	public static Player player2;
+	private static Infiltrant infiltrant;//j'appelle le joueur1 dans Game
+	public static Gardien gardien;
 	private static Timer timer;//j'ai besoin du chrono dans Game
-
-		
-	public static void move (Player player1){
-		int versX = player1.getX();// je prend en memoire la case ou je vais suivant x
-		int versY = player1.getY(); // idem suivant y
-		
-		
-		if (StdDraw.isKeyPressed(KeyEvent.VK_Z)){
-			versY = versY -1;
-		}
-		if (StdDraw.isKeyPressed(KeyEvent.VK_S)){
-			versY = versY +1;
-		}
-		if (StdDraw.isKeyPressed(KeyEvent.VK_D)){
-			versX = versX +1;
-		}
-		if (StdDraw.isKeyPressed(KeyEvent.VK_Q)){
-			versX = versX -1;
-		}
-				
-		if(Map.map[versX][versY] == 0 || Map.map[versX][versY] == 3 || Map.map[versX][versY] == 4) //condition, si c'est un mur alors ne rien faire (pas de getteur, ni de setteur)
-			return;//ATTENTION ERREUR [versX][versY] position en direct --> une fois que je suis dans un mur ça ne marche plus (comme prevu / or je veux delimitter les murs
-			
-			
-			//ERREUR ! Soit les murs soit les limites ! 
-				
-		if (versX >=0 && versX <= scale * Map.map[0].length)//je donne les limites de la map (horizontalement de part et d'autre de la map)
-			player1.setX(versX);
-		
-		
-		if (versY >=0 && versY < Map.map[0].length)//je donne les limites de la map (verticalement de part et d'autre de la map)
-			player1.setY(versY);
-		
-
-		}
-	
-
-/* public void eteindre (Map map1){
-		if (StdDraw.isKeyPressed(KeyEvent.VK_SPACE)){
-			//Je declare que si j'appuie sur espace alors le boolean devient "vrai" et donc la map devient noir
-			
-			Map.espace = true;
-			map.switchOff();
-		}
-	} */
-	
-	
-	
-public static void move1 (Player player2){
-		int versX = player2.getX();// je prend en memoire la case ou je vais suivant x
-		int versY = player2.getY(); // idem suivant y
-		
-		
-		if (StdDraw.isKeyPressed(KeyEvent.VK_UP)){
-			versY = versY -1;
-		}
-		if (StdDraw.isKeyPressed(KeyEvent.VK_DOWN)){
-			versY = versY +1;
-		}
-		if (StdDraw.isKeyPressed(KeyEvent.VK_RIGHT)){
-			versX = versX +1;
-		}
-		if (StdDraw.isKeyPressed(KeyEvent.VK_LEFT)){
-			versX = versX -1;
-		}
-				
-		if(Map.map[versX][versY] == 0 || Map.map[versX][versY] == 3 || Map.map[versX][versY] == 4) //condition, si c'est un mur alors ne rien faire (pas de getteur, ni de setteur)
-			return;//ATTENTION ERREUR [versX][versY] position en direct --> une fois que je suis dans un mur ça ne marche plus (comme prevu / or je veux delimitter les murs
-			
-				
-		if (versX >=0 || versX <= scale * Map.map[0].length){//je donne les limites de la map (horizontalement de part et d'autre de la map)
-			player2.setX(versX);
-	}
-		if (versY >=0 || versY < Map.map[0].length){//je donne les limites de la map (verticalement de part et d'autre de la map)
-			player2.setY(versY);
-			}
-
-				
-	} 
-		
 	
 		
-public static void main(String[] args) throws InterruptedException { //throws InterruptedException necessaire pour "Thread"
+	public Game() throws InterruptedException{
 		StdDraw.setCanvasSize(2*120 + X_MAX * (scale +1 ), Y_MAX * (scale +1)); //permet de modifier la taille de la fenetre
 		StdDraw.setXscale(0, X_MAX + 3*WIDTH ); //permet de définir la taille de notre map (suivant X)
 		StdDraw.setYscale(0, Y_MAX );//permet de definir la taille de notre map (suivant Y)
 		
 		map = new Map ();
-		player1 = new Player (1,1);
-		player2 = new Player (15,1);
+		infiltrant = new Infiltrant (1,1);
+		gardien = new Gardien (15,1);
 		timer = new Timer();
 		
 		
@@ -110,11 +28,11 @@ public static void main(String[] args) throws InterruptedException { //throws In
 		while(true){//boucle infinie
 			timer.start(); //on lance le chrono
 			
-			move(player1);
-			move1(player2);
+			Infiltrant.move(infiltrant);
+			Gardien.move(gardien);
 			map.drawMap(); //je dessine la map
-			player1.drawPlayer(); //je dessine le joueur par dessus la carte
-			player2.drawPlayer1();
+			infiltrant.drawPlayer(); //je dessine le joueur par dessus la carte
+			gardien.drawPlayer();
 			
 			if (StdDraw.isKeyPressed(KeyEvent.VK_SPACE)){
 			map.switchOff();
@@ -127,13 +45,13 @@ public static void main(String[] args) throws InterruptedException { //throws In
 			long time = 190 - timer.getElapsedTime();  //sensibilité, temps de reponse à partir du moment ou on lui ordonne une action
 			if (time < 190){
 				Thread.sleep(time); //pause || thread = fill
-			}
-		}
 	}
-
-
-
-
-
-
+			}
+	}
+	
+	public static void main(String[] args) throws InterruptedException { //throws InterruptedException necessaire pour "Thread"
+		new Game( );
+			}
+		
+	
 }
---
+++ LaDisparition2/src/Gardien.java
@@ -0,0 +1,82 @@
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+
+
+public class Gardien {
+		private int x; //coordonne de mon joueur suivant x
+		private int y;//coordonne suivant y
+		
+		public Gardien (int x, int y){//constructeur de ma classe Player
+			this.x = x;
+			this.y = y;
+		}
+
+		//mes "getteur"
+		public int getX(){ // je recupere (garder en memoire) le x de mon personnage
+			return x;
+		}
+		public int getY(){// je recupere (garder en memoire) le y de mon personnage
+			return y;
+		}
+		
+		//mes "setteur"
+		public void setX(int value){
+			x = value;
+		}
+		public void setY(int value){
+			y = value;
+		}
+
+
+	public void drawPlayer(){ //methode qui me permet de dessiner mon joueur
+		StdDraw.setPenColor(Color.PINK);
+		
+		int x = (int)Game.WIDTH * getX();
+		int y = Game.Y_MAX - (int)Game.WIDTH * getY();
+		//int r = (int)Game.WIDTH / 2;
+		
+		
+		 StdDraw.picture(x,y,"perso2.gif");
+		 
+		 
+		
+		//StdDraw.filledCircle(x, y, r);
+		
+		}
+
+	public static void move (Gardien player){
+		int versX = player.getX();// je prend en memoire la case ou je vais suivant x
+		int versY = player.getY(); // idem suivant y
+		
+		
+		if (StdDraw.isKeyPressed(KeyEvent.VK_UP)){
+			versY = versY -1;
+		}
+		if (StdDraw.isKeyPressed(KeyEvent.VK_DOWN)){
+			versY = versY +1;
+		}
+		if (StdDraw.isKeyPressed(KeyEvent.VK_RIGHT)){
+			versX = versX +1;
+		}
+		if (StdDraw.isKeyPressed(KeyEvent.VK_LEFT)){
+			versX = versX -1;
+		}
+				
+		if(Map.map[versX][versY] == 0 || Map.map[versX][versY] == 3 || Map.map[versX][versY] == 4) //condition, si c'est un mur alors ne rien faire (pas de getteur, ni de setteur)
+			return;//ATTENTION ERREUR [versX][versY] position en direct --> une fois que je suis dans un mur a ne marche plus (comme prevu / or je veux delimitter les murs
+			
+			
+			//ERREUR ! Soit les murs soit les limites ! 
+				
+		if (versX >=0 && versX <= Game.scale * Map.map[0].length)//je donne les limites de la map (horizontalement de part et d'autre de la map)
+			player.setX(versX);
+		
+		
+		if (versY >=0 && versY < Map.map[0].length)//je donne les limites de la map (verticalement de part et d'autre de la map)
+			player.setY(versY);
+		
+
+		}
+
+	
+}
